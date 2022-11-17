7da2c8146c27a513134540ec746dc6f86f9736e2
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2022-11-17 16:55:15 -0500

    login funcional añadido, necesita aplicarse herencia para las clases configuracion registro e iniciosesion

---
+++ src/procesos/inicioSesionConfig.java
@@ -0,0 +1,52 @@
+package procesos;
+
+import entidades.Chofer;
+import entidades.Pasajero;
+
+import java.util.Enumeration;
+
+public class inicioSesionConfig extends configuracion {
+    public void creaChofer(Chofer chof){
+        Enumeration<Chofer> cf = this.getChoferes().elements();
+        while(cf.hasMoreElements()) {
+            Chofer chofer = cf.nextElement();
+        }
+        this.getChoferes().add((Chofer) chof);
+    }
+    public void creaPasajero(Pasajero pas){
+        Enumeration<Pasajero> psj = this.getPasajeros().elements();
+        while (psj.hasMoreElements()){
+            Pasajero pasa = psj.nextElement();
+        }
+        this.getPasajeros().add((Pasajero) pas);
+    }
+    public int IniciarSesionPasajero(String email, String contra) {
+        Enumeration<Pasajero> pas = this.getPasajeros().elements();
+        while (pas.hasMoreElements()) {
+            Pasajero pasajero = pas.nextElement();
+            String email1  = String.valueOf(pasajero.getEmail());
+            System.out.println(getChoferes());
+            if (email1.equals(email)) {
+                System.out.println("Usuario detectado: " + pasajero.NombreCompleto());
+                if (pasajero.getContrasenia().equals(contra)) {
+                    System.out.println("Bievenido: " + pasajero.NombreCompleto());
+                    return 1;
+                }
+            }
+        }
+        System.out.println("Contraseña incorrecta");
+        return 0;
+    }
+    public int IniciarSesionChofer(String email, String contra) {
+        Enumeration<Chofer> cf = this.getChoferes().elements();
+        while (cf.hasMoreElements()) {
+            Chofer chofer = cf.nextElement();
+            if (chofer.getEmail().equals(email) && chofer.getContrasenia().equals(contra)) {
+                System.out.println("Bievenido"+chofer.NombreCompleto());
+                return 1;
+            }
+        }
+        System.out.println("Contraseña incorrecta");
+        return 0;
+    }
+}
