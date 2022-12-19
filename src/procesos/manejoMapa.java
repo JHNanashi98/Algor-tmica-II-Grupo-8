129befecb3fedcb79ecfa9605c5aa1a56d230e66
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2022-12-18 22:27:21 -0500

    error en el merge

--- src/procesos/manejoMapa.java
+++ src/procesos/manejoMapa.java
@@ -14,21 +14,12 @@ import javax.swing.event.MouseInputListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Scanner;
-<<<<<<< HEAD
 public class manejoMapa{
     public void crearMapa() {
         TileFactoryInfo info = new OSMTileFactoryInfo();
         DefaultTileFactory tile = new DefaultTileFactory(info);
         ventanaKeyCab ventana = new ventanaKeyCab();
         JXMapViewer mapa = ventana.getMapa();
-=======
-public class manejoMapa extends configuracion{
-    public void crearMapa(configuracion config) {
-        TileFactoryInfo info = new OSMTileFactoryInfo();
-        DefaultTileFactory tile = new DefaultTileFactory(info);
-        ventanaKeyCab ventana = new ventanaKeyCab();
-        final JXMapViewer mapa = ventana.getMapa();
->>>>>>> e8e993fce05d34e34df1c43245e4df399a9873e4
         mapa.setTileFactory(tile);
         geoLocalizacion geo = new geoLocalizacion();
         mapa.setAddressLocation(geo.consegirUbicacion(-12.06215203252874, -77.03681945800781));
@@ -38,26 +29,14 @@ public class manejoMapa extends configuracion{
         do {
             System.out.println("Use el click izquierdo para seleccionar la posicion inicial");
             System.out.println("Use el click derecho para seleccionar la posicion final");
-<<<<<<< HEAD
             GeoPosition[] geoI = selecCoordsInicio(mapa);
             GeoPosition[] geoF = selecCoordsFin(mapa);
-=======
-            GeoPosition[] geoI = selecCoordsInicio(mapa, config);
-            GeoPosition[] geoF = selecCoordsFin(mapa, config);
->>>>>>> e8e993fce05d34e34df1c43245e4df399a9873e4
             Scanner s = new Scanner(System.in);
             System.out.println("Confirmar Viaje S/N");
             op = s.next().charAt(0);
             if (op == 'S') {
-<<<<<<< HEAD
                 System.out.println(geoI[0]);
                 System.out.println(geoF[0]);
-=======
-                for(int i=0;i<geoF.length; i++) {
-                    System.out.println(geoI[i]);
-                    System.out.println(geoF[i]);
-                }
->>>>>>> e8e993fce05d34e34df1c43245e4df399a9873e4
             }
         }while(op != 'S');
     }
@@ -68,11 +47,7 @@ public class manejoMapa extends configuracion{
         mapa.addMouseMotionListener(mouse);
         mapa.addMouseWheelListener(zoom);
     }
-<<<<<<< HEAD
     public GeoPosition[] selecCoordsInicio(JXMapViewer mapa) {
-=======
-    public GeoPosition[] selecCoordsInicio(JXMapViewer mapa, configuracion config) {
->>>>>>> e8e993fce05d34e34df1c43245e4df399a9873e4
         GeoPosition [] arregloPos = new GeoPosition[1];
         mapa.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
@@ -87,11 +62,7 @@ public class manejoMapa extends configuracion{
         });
         return arregloPos;
     }
-<<<<<<< HEAD
     public GeoPosition[] selecCoordsFin(JXMapViewer mapa) {
-=======
-    public GeoPosition[] selecCoordsFin(JXMapViewer mapa, configuracion config) {
->>>>>>> e8e993fce05d34e34df1c43245e4df399a9873e4
         GeoPosition [] arregloPos = new GeoPosition[1];
         mapa.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
