e8e993fce05d34e34df1c43245e4df399a9873e4
Joaquin Hidalgo <113700898+JHNanashi98@users.noreply.github.com>
2022-12-18 21:39:45 -0500

    Se ha agregado sistema para selecionar y almacenar las coordendas (#15)
    * eliminado codigo comentado y vector chofer inecesario; agregada funcionalidad basica mapa
    
    * merging branches
    
    * agregada funcionalidad para mostrar datos
    
    * simplificado procesos, y eliminada clase de inicarsesion los metodos de esta fueron remplazados por polimorfismo
    
    * agregado sistema para seleccionar coordenadas utilizando el mouse
    
    * agregado sistema para selecioanr coordenadas usando el mouse

--- src/entidades/Viaje.java
+++ src/entidades/Viaje.java
@@ -4,8 +4,8 @@ public class Viaje {
     private String codViaje;
     private Chofer chofer;
     private Pasajero usuario;
-    private Ruta ruta;
     private Double monto;
+    private Double duracion;
     private boolean completado;
     public boolean getCompletado(){
         return completado;
--- src/procesos/manejoMapa.java
+++ src/procesos/manejoMapa.java
@@ -4,27 +4,44 @@ import mapeo.ventanaKeyCab;
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.OSMTileFactoryInfo;
 
-import org.jxmapviewer.input.MapClickListener;
 import org.jxmapviewer.input.PanMouseInputListener;
 import org.jxmapviewer.input.ZoomMouseWheelListenerCenter;
-import org.jxmapviewer.viewer.*;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
 
 import javax.swing.event.MouseInputListener;
-
-public class manejoMapa {
-    public void crearMapa() {
-        ventanaKeyCab ventana = new ventanaKeyCab();
-        JXMapViewer mapa = ventana.getMapa();
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Scanner;
+public class manejoMapa extends configuracion{
+    public void crearMapa(configuracion config) {
         TileFactoryInfo info = new OSMTileFactoryInfo();
         DefaultTileFactory tile = new DefaultTileFactory(info);
-        geoLocalizacion geo = new geoLocalizacion();
+        ventanaKeyCab ventana = new ventanaKeyCab();
+        final JXMapViewer mapa = ventana.getMapa();
         mapa.setTileFactory(tile);
+        geoLocalizacion geo = new geoLocalizacion();
         mapa.setAddressLocation(geo.consegirUbicacion(-12.06215203252874, -77.03681945800781));
         mapa.setZoom(8);
         moverMapa(mapa);
-        selecionarCoords(mapa);
+        char op;
+        do {
+            System.out.println("Use el click izquierdo para seleccionar la posicion inicial");
+            System.out.println("Use el click derecho para seleccionar la posicion final");
+            GeoPosition[] geoI = selecCoordsInicio(mapa, config);
+            GeoPosition[] geoF = selecCoordsFin(mapa, config);
+            Scanner s = new Scanner(System.in);
+            System.out.println("Confirmar Viaje S/N");
+            op = s.next().charAt(0);
+            if (op == 'S') {
+                for(int i=0;i<geoF.length; i++) {
+                    System.out.println(geoI[i]);
+                    System.out.println(geoF[i]);
+                }
+            }
+        }while(op != 'S');
     }
-
     public void moverMapa(JXMapViewer mapa) {
         MouseInputListener mouse = new PanMouseInputListener(mapa);
         ZoomMouseWheelListenerCenter zoom = new ZoomMouseWheelListenerCenter(mapa);
@@ -32,13 +49,35 @@ public class manejoMapa {
         mapa.addMouseMotionListener(mouse);
         mapa.addMouseWheelListener(zoom);
     }
-    public void selecionarCoords(JXMapViewer mapa) {
-        MapClickListener mouse = new MapClickListener(mapa) {
-            @Override
-            public void mapClicked(GeoPosition geo) {
-                System.out.println(geo);
-                System.out.println("X:"+geo.getLatitude()+",Y:"+geo.getLongitude());
+    public GeoPosition[] selecCoordsInicio(JXMapViewer mapa, configuracion config) {
+        GeoPosition [] arregloPos = new GeoPosition[1];
+        mapa.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 1 && e.getButton() == MouseEvent.BUTTON1) {
+                    java.awt.Point p = e.getPoint();
+                    GeoPosition geo = mapa.convertPointToGeoPosition(p);
+                    System.out.println("Punto de partida");
+                    System.out.println("X:" + geo.getLatitude() + ",Y:" + geo.getLongitude());
+                    arregloPos[0] = geo;
+                }
             }
-        };
+        });
+        return arregloPos;
+    }
+    public GeoPosition[] selecCoordsFin(JXMapViewer mapa, configuracion config) {
+        GeoPosition [] arregloPos = new GeoPosition[1];
+        mapa.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 1 && e.getButton() == MouseEvent.BUTTON3) {
+                    java.awt.Point p = e.getPoint();
+                    GeoPosition geo = mapa.convertPointToGeoPosition(p);
+                    System.out.println("Punto de Llegada");
+                    System.out.println("X:" + geo.getLatitude() + ",Y:" + geo.getLongitude());
+                    arregloPos[0] = geo;
+                }
+            }
+        });
+        return arregloPos;
     }
 }
+
